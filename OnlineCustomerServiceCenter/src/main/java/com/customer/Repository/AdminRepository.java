ba5774ddbf3637030dd7301fa6e68dc993420cf9
NarendraKumarswami <narendrakumarswami786@gmail.com>
2023-03-30 14:14:17 +0530

    Revert "day2 updates"
    This reverts commit 9de4c5b70644801b61927f1672da0fde3eca321a.

--- OnlineCustomerServiceCenter/src/main/java/com/customer/Repository/AdminRepository.java
+++ OnlineCustomerServiceCenter/src/main/java/com/customer/Repository/AdminRepository.java
@@ -1,16 +1,10 @@
 package com.customer.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import com.customer.Entity.Admin;
-
-@Repository
-public interface AdminRepository extends JpaRepository<Admin, Integer>{
+public interface AdminRepository {
 	
 	
 	
-	public Admin findByEmail();
 	
 
 }
--- OnlineCustomerServiceCenter/src/main/java/com/customer/Repository/DepartmentDao.java
+++ OnlineCustomerServiceCenter/src/main/java/com/customer/Repository/DepartmentDao.java
@@ -1,11 +1,6 @@
 package com.customer.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.customer.Entity.Department;
+public interface DepartmentDao {
 
-public interface DepartmentDao extends JpaRepository<Department, Integer>	 {
-        
-	
-	   
 }
