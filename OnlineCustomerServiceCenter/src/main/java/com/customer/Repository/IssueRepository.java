7dcd035542bafab85e1c9e9f2411aa6b105a7195
naveensharma8527 <nsnaveensharma1998@gmail.com>
2023-03-30 15:31:38 +0530

    issueRepo

--- OnlineCustomerServiceCenter/src/main/java/com/customer/Repository/IssueRepository.java
+++ OnlineCustomerServiceCenter/src/main/java/com/customer/Repository/IssueRepository.java
@@ -2,8 +2,12 @@ package com.customer.Repository;
 
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IssueRepository {
+import com.customer.Entity.Issue;
+
+
+public interface IssueRepository extends JpaRepository<Issue, Integer> {
 	
 
 
